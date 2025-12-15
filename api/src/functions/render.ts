f2c9c067e4476a8a3550f7b3c5da7b3d77d757e6
shadow <shadow8095@gmail.com>
2025-12-15 15:37:49 -0500

    asdasd

--- api/src/functions/render.ts
+++ api/src/functions/render.ts
@@ -3,10 +3,9 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 export async function render(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
 
     return { 
-        body: `<html><body><h1>Hello from API, ${name}!</h1></body></html>`,
+        body: `<html><body><h1>Hello from API route man!</h1></body></html>`,
         headers: {
             'Content-Type': 'text/html'
         }
